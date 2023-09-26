d9ccccf1e1f58c63226284f617dba47605c75422
Muhammad Islam <muhammad.islam@kcl.ac.uk>
2023-09-26 16:48:53 +0100

    Make median function
    Used the statistics package to solve the problem but also made my own function called 'median' to give the same solution.

--- median.py
+++ median.py
@@ -1,12 +1,28 @@
 """Median calculator."""
 """ENTER YOUR SOLUTION HERE!"""
+import statistics
+
+
+def median(list):
+    list.sort()
+    if len(list) % 2 == 1:
+        return list[len(list) // 2]
+    else:
+        midValue = len(list) // 2
+        num1 = list[midValue-1]
+        num2 = list[midValue ]
+        median = (num1 + num2) / 2
+        return median
 
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
+        print(statistics.median(numbers))
+        median = median(numbers)
+        print(f'The median value is {median}')
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+#print(numbers)
